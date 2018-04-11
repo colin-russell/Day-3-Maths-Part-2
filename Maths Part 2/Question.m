f33ed512b40171adafa09f2c9ad2ad703afe3e84
Colin Russell <colinrussell101@gmail.com>
2018-04-11 18:24:05 -0400

    cleanup

--- Maths Part 2/Question.m
+++ Maths Part 2/Question.m
@@ -12,14 +12,9 @@
 
 - (instancetype) init {
     if (self = [super init]) {
-//        int a = arc4random_uniform(90)+10;
-//        int b = arc4random_uniform(90)+10;
         
         _leftValue = arc4random_uniform(91)+11;
         _rightValue = arc4random_uniform(91)+11;
-        
-//        _question = [NSString stringWithFormat:@"%d + %d ?", a, b];
-//        _answer = a+b;
         
         // set startTime to the current date
         _startTime = [NSDate date];
